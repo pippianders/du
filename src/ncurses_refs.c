pippianders/du

commit 1548f9276f48b9d7a378b2e491609fc947f93e6c

Fix type signature of ncdu_init_pair()

src/ncurses_refs.c
@@ -27,4 +27,4 @@ chtype ncdu_acs_hline()    { return ACS_VLINE   ; }
 chtype ncdu_acs_vline()    { return ACS_HLINE   ; }
 
 /* https://github.com/ziglang/zig/issues/8947 */
-void ncdu_init_pair(short a,b,c) { init_pair(a,b,c); }
+void ncdu_init_pair(short a, short b, short c) { init_pair(a,b,c); }

commit 0d16b9f33e875548ca2715d399494da7adbe7688

Fix colors on FreeBSD (and MacOS?) again
Broken in 1548f9276f48b9d7a378b2e491609fc947f93e6c because I'm an idiot.

Probably also fixes #210, but I don't have a Mac to test.

src/ncurses_refs.c
@@ -27,4 +27,4 @@ chtype ncdu_acs_hline()    { return ACS_VLINE   ; }
 chtype ncdu_acs_vline()    { return ACS_HLINE   ; }
 
 /* https://github.com/ziglang/zig/issues/8947 */
-void ncdu_init_pair(short a, short b, short c) { init_pair(a,b,c); }
+void ncdu_init_pair(int a, int b, int c) { init_pair(a,b,c); }

src/ui.zig
@@ -197,7 +197,7 @@ extern fn ncdu_acs_urcorner() c.chtype;
 extern fn ncdu_acs_lrcorner() c.chtype;
 extern fn ncdu_acs_hline()    c.chtype;
 extern fn ncdu_acs_vline()    c.chtype;
-extern fn ncdu_init_pair(idx: c_short, fg: c_short, bg: c_short) void;
+extern fn ncdu_init_pair(idx: c_int, fg: c_int, bg: c_int) void;
 
 const StyleAttr = struct { fg: i16, bg: i16, attr: u32 };
 const StyleDef = struct {